d7a171a3be40c4a191b54c3d411b5c4e12819da9
Mac <mac@abctech-thailand.com>
2013-07-07 21:48:07 +0700

    #26 Implement Facebook login

--- src/main/java/com/punmac/footballmatchup/webapp/controller/DefaultController.java
+++ src/main/java/com/punmac/footballmatchup/webapp/controller/DefaultController.java
@@ -2,6 +2,7 @@ package com.punmac.footballmatchup.webapp.controller;
 
 import com.punmac.footballmatchup.core.dao.PlayerDao;
 import com.punmac.footballmatchup.core.model.Player;
+import com.punmac.footballmatchup.webapp.bean.FacebookUser;
 import com.punmac.footballmatchup.webapp.bean.form.LoginForm;
 import com.punmac.footballmatchup.webapp.bean.form.RegisterForm;
 import com.punmac.footballmatchup.webapp.util.CookieSessionUtil;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -36,6 +38,9 @@ public class DefaultController {
 
     @Autowired
     private LoginValidator loginValidator;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping(value = {"/", "home"})
     public String home() {
@@ -79,6 +84,33 @@ public class DefaultController {
         }
         model.addAttribute("pageTitle", "Sign In");
         model.addAttribute("pageContent", "default/login");
+        return "layout";
+    }
+
+    @RequestMapping(value = "login/fb")
+    public String loginFB(Model model, HttpServletRequest request) {
+        if(request.getParameter("error") != null) { // If fb login not success.
+            return "redirect:/";
+        }
+
+        String code = request.getParameter("code");
+        log.debug("code = {}", code);
+
+        String token;
+        String queryStringToken = restTemplate.getForObject("https://graph.facebook.com/oauth/access_token?" +
+                "client_id=517466198319051" +
+                "&redirect_uri=" + request.getRequestURL().toString().replace(request.getRequestURI(), "") + "/login/fb" +
+                "&client_secret=aaefacaf6a650c7b893f130044b46269" +
+                "&code=" + code, String.class);
+        log.debug("jsonToken = {}", queryStringToken);
+        String[] splitQueryStringToken = queryStringToken.split("&");
+        String[] splitToken = splitQueryStringToken[0].split("=");
+        token = splitToken[1];
+        log.debug("token = {}", token);
+
+        FacebookUser facebookUser = restTemplate.getForObject("https://graph.facebook.com/me?fields=name,username,email&access_token=" + token, FacebookUser.class);
+        log.debug("user info = {}", facebookUser.toString());
+        model.addAttribute("pageContent", "test");
         return "layout";
     }
 
