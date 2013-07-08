b756d52d90f42db68114179afdb31e698725407d
Mac <mac@abctech-thailand.com>
2013-07-08 17:59:59 +0700

    Try fixed NPE in server

--- src/main/java/com/punmac/footballmatchup/webapp/util/CookieSessionUtil.java
+++ src/main/java/com/punmac/footballmatchup/webapp/util/CookieSessionUtil.java
@@ -5,6 +5,8 @@ import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.punmac.footballmatchup.core.model.Player;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public final class CookieSessionUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(CookieSessionUtil.class);
 
     private static final String COOKIE_SESSION_NAME = "player";
     private static final int COOKIE_MAX_AGE = 2628000; // 1 month.
@@ -101,6 +105,12 @@ public final class CookieSessionUtil {
      * @return Cookie
      */
     public static Cookie getCookie(HttpServletRequest request, String name) {
+        log.debug("Request = {}", request);
+        if(request == null) {
+            return null;
+        }
+        log.debug("Cookie = {}", request.getCookies());
+        log.debug("Cookie Size = {}", request.getCookies().length);
         for(Cookie cookie : request.getCookies()) {
             if(name.equals(cookie.getName())) {
                 return cookie;
